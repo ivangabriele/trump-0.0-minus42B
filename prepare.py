7464c349995b3823e536db3ee5b71d9b4d1c1452
Ivan Gabriele <ivan.gabriele@protonmail.com>
2025-06-18 00:20:55 +0200

    feat(prepare): simplify steps

--- prepare.py
+++ prepare.py
@@ -1,9 +1,8 @@
 from peft import LoraConfig
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoModelForSequenceClassification, AutoTokenizer
 import torch
 from transformers.generation.configuration_utils import GenerationConfig
 from trl import (  # type: ignore
-    AutoModelForCausalLMWithValueHead,
     PPOTrainer,
     PPOConfig,
     RewardTrainer,
@@ -17,9 +16,7 @@ from datasets import Dataset  # type: ignore
 from libs import preference_dataset_manager
 
 
-BATCH_SIZE = 4
-NUM_EPOCHS = 3
-LEARNING_RATE = 1.41e-5
+LEARNING_RATE = 3e-6
 
 
 def _to_reward_model_dataset(dataset: PreferenceDataset) -> Dataset:
@@ -37,41 +34,36 @@ def _to_reward_model_dataset(dataset: PreferenceDataset) -> Dataset:
     return Dataset.from_list(pairs)
 
 
-def _train_reward_model():
+def _train_reward_model(should_train: bool) -> tuple:
     print("Info: Training Reward Model...")
 
     preference_dataset = preference_dataset_manager.read()
     reward_model_dataset = _to_reward_model_dataset(preference_dataset)
 
-    tokenizer = AutoTokenizer.from_pretrained(REWARD_MODEL)
-    # **Crucially, also set it in the model's configuration**
-    # DistilBERT does not have a default pad token, so we set it to the unk token
-    # which is a standard practice for this model architecture.
-    if tokenizer.pad_token is None:
-        tokenizer.pad_token = tokenizer.unk_token
+    reward_tokenizer = AutoTokenizer.from_pretrained(REWARD_MODEL)
+    reward_tokenizer.add_special_tokens({"pad_token": "[PAD]"})
 
-    model = AutoModelForSequenceClassification.from_pretrained(
-        REWARD_MODEL, num_labels=1, torch_dtype=torch.bfloat16, device_map="auto"
-    )
+    reward_model = AutoModelForSequenceClassification.from_pretrained(REWARD_MODEL, trust_remote_code=False)
 
     # https://huggingface.co/docs/trl/v0.18.1/reward_trainer#trl.RewardConfig
     training_args = RewardConfig(
         eval_strategy="no",
         learning_rate=LEARNING_RATE,
         logging_steps=10,
-        num_train_epochs=NUM_EPOCHS,
+        num_train_epochs=3,
         output_dir=REWARD_MODEL_DIR_PATH,
-        per_device_train_batch_size=BATCH_SIZE,
+        per_device_train_batch_size=1,
         remove_unused_columns=False,
         save_strategy="epoch",
     )
     trainer = RewardTrainer(
-        model=model, args=training_args, processing_class=tokenizer, train_dataset=reward_model_dataset
+        model=reward_model, args=training_args, processing_class=reward_tokenizer, train_dataset=reward_model_dataset
     )
-    trainer.train()
-    trainer.save_model()
+    if should_train:
+        trainer.train()
+        trainer.save_model()
 
-    return model, tokenizer
+    return reward_model, reward_tokenizer
 
 
 def _to_ppo_dataset(dataset: PreferenceDataset) -> Dataset:
@@ -87,27 +79,18 @@ def _train_with_ppo(reward_model, reward_tokenizer):
     ppo_dataset = _to_ppo_dataset(preference_dataset)
 
     tokenizer = AutoTokenizer.from_pretrained(GENERATOR_MODEL)
-    tokenizer.pad_token = tokenizer.eos_token
-
-    # We must tokenize the dataset before passing it to the PPOTrainer.
-    # The dataloader needs the 'input_ids' column.
-    def tokenize(example):
-        example["input_ids"] = tokenizer(example["query"], truncation=True).input_ids
-
-        return example
-
-    tokenized_ppo_dataset = ppo_dataset.map(tokenize, batched=False, remove_columns=["query"])
+    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
 
     generation_config = GenerationConfig.from_pretrained(GENERATOR_MODEL)
     generation_config.pad_token_id = tokenizer.pad_token_id
-    model = AutoModelForCausalLMWithValueHead.from_pretrained(
+    model = AutoModelForCausalLM.from_pretrained(
         GENERATOR_MODEL, attn_implementation="eager", torch_dtype=torch.bfloat16, device_map="auto"
     )
     model.generation_config = generation_config
     model.base_model_prefix = "pretrained_model"
 
-    ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(
-        GENERATOR_MODEL, attn_implementation="eager", torch_dtype=torch.bfloat16, device_map="auto"
+    value_model = AutoModelForSequenceClassification.from_pretrained(
+        REWARD_MODEL, trust_remote_code=False, num_labels=1
     )
 
     # Configure LoRA (Low-Rank Adaptation) PEFT (Parameter-Efficient Fine-Tuning)
@@ -120,21 +103,21 @@ def _train_with_ppo(reward_model, reward_tokenizer):
         target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
     )
     ppo_config = PPOConfig(
-        batch_size=BATCH_SIZE,
-        # gradient_accumulation_steps=4,
+        batch_size=64,
+        gradient_accumulation_steps=1,
         learning_rate=LEARNING_RATE,
-        mini_batch_size=1,
-        # per_device_train_batch_size=1,
+        missing_eos_penalty=1.0,
+        per_device_train_batch_size=64,
         reward_model_path=GENERATOR_MODEL,
     )
     ppo_trainer = PPOTrainer(
         args=ppo_config,
         model=model,
-        ref_model=ref_model,
+        ref_model=None,
         reward_model=reward_model,
-        value_model=model,
+        value_model=value_model,
         processing_class=tokenizer,
-        train_dataset=tokenized_ppo_dataset,
+        train_dataset=ppo_dataset,
         peft_config=lora_config,
     )
 
@@ -159,7 +142,7 @@ def main():
         message=".*'pin_memory' argument is set as true but no accelerator is found.*",
     )
 
-    reward_model, reward_tokenizer = _train_reward_model()
+    reward_model, reward_tokenizer = _train_reward_model(False)
     _train_with_ppo(reward_model, reward_tokenizer)
 
 
